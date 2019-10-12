9d93d77bb942d2b57a6b569c2a8fd6b513fdd050
alami <alami@km.ru>
2019-10-12 20:11:16 +0500

    2-4.1

---
+++ src/personalfinance/model/Article.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalfinance.model;
+
+/**
+ *
+ * @author user
+ */
+public class Article extends Common{
+    private String title;
+    public Article(){}
+}
