2068bbbf34b8775caa4470b0725d080e470d2723
alami <alami@km.ru>
2019-10-12 19:07:21 +0500

    2-2. create packet Settings

--- src/personalfinance/PersonalFinance.java
+++ src/personalfinance/PersonalFinance.java
@@ -5,6 +5,16 @@
  */
 package personalfinance;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import personalfinance.settings.Text;
+
 /**
  *
  * @author user
@@ -15,7 +25,21 @@ public class PersonalFinance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.err.println("Personal Finance ! ");
+        init();
+        System.out.println("Personal Finance ! "
+                + Text.get("PROGRAM_NAME")+" ! "
+                + Arrays.toString(Text.getMonths()));
+    }
+    private static void init() {
+        try {
+            Text.init();
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Roboto-Light.ttf")));
+        } catch (FontFormatException ex) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- src/personalfinance/settings/Text.java
+++ src/personalfinance/settings/Text.java
@@ -13,6 +13,25 @@ import java.util.HashMap;
  */
 final public class Text {
     private static final HashMap<String, String> data = new HashMap();
+    public static String get(String key) {
+        return data.get(key);        
+    }
+    public static String[] getMonths () {
+        String[] months = new String[12];
+        months[0] = get("JANUARY");
+        months[1] = get("FEBRUAR");
+        months[2] = get("MARCH");
+        months[3] = get("APRIL");
+        months[4] = get("MAY");
+        months[5] = get("JUNE");
+        months[6] = get("JULY");
+        months[7] = get("AUGUST");
+        months[8] = get("SEPTEMBER");
+        months[9] = get("OCTOBER");
+        months[10] = get("NOVEMBER");
+        months[11] = get("DECEMBER");
+        return months;
+    }
     public static void init () {
         data.put("PROGRAM_NAME", "Домашняя бухгалтерия");
 
